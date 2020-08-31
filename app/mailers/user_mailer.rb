b510e99de77b9efc28769174bb6fa6148792e832
ALFOLDI <balfoldi@yahoo.fr>
2020-08-31 16:25:19 +0200

    mailer should be implemented

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   default from: 'no-reply@monsite.fr'
-  def welcome_email(user)
+  def bill_email(user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @user = user 
 
