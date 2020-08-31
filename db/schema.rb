df6dc7ac959004a81ce24c04e40b74deca954d62
Lucas <lucas.gautier@live.fr>
2020-08-31 16:18:05 +0200

    premier commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_08_31_080323) do
-=======
-ActiveRecord::Schema.define(version: 2020_08_31_100548) do
->>>>>>> master
+ActiveRecord::Schema.define(version: 2020_08_31_115330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "indices", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "media", force: :cascade do |t|
     t.string "title"
@@ -28,8 +29,6 @@ ActiveRecord::Schema.define(version: 2020_08_31_100548) do
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
-=======
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -42,5 +41,4 @@ ActiveRecord::Schema.define(version: 2020_08_31_100548) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
->>>>>>> master
 end
