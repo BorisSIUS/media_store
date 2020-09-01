67970a3e07fdb6f7a856b95325db41df74090898
Lucas <lucas.gautier@live.fr>
2020-09-01 17:57:35 +0200

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,8 @@ ActiveRecord::Schema.define(version: 2020_09_01_120901) do
     t.bigint "media_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_carts_on_user_id", unique: true
+    t.index ["cart_id"], name: "index_join_carts_medias_on_cart_id"
+    t.index ["media_id"], name: "index_join_carts_medias_on_media_id"
   end
 
   create_table "media", force: :cascade do |t|
