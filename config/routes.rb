28127e8582514f4e8ded7481a9de423466665938
Lucas <lucas.gautier@live.fr>
2020-09-01 14:40:48 +0200

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   root 'medias#index'
 
   resources :medias
-  
+  resources :medias do 
+    resources :carts
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
