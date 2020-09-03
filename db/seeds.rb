464e1df3ea044e710824682c0fd0c821f26cde60
ALFOLDI <balfoldi@yahoo.fr>
2020-09-03 11:55:20 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,6 @@ end
 
 User.create(email: "bobo@yopmail.com", password: "123123")
 
-User.each do 
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
