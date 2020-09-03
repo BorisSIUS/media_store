01bffabbabfb18747760a548ee8811bfe165eb5f
ALFOLDI <balfoldi@yahoo.fr>
2020-09-03 13:53:59 +0200

    done

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,15 +1,15 @@
-class UserMailer < ApplicationMailer
+class OrderMailer < ApplicationMailer
   default from: 'balfoldi@yahoo.fr'
   
   def admin_bill_email(user)
-    @cart = User.cart
+    @cart = user.cart
     @user = user
     @url  = 'https://pastek-media-store.herokuapp.com/' 
     mail(to: "master@yopmail.com", subject: 'Un utilisateur a passé une commande!') 
   end
 
   def user_bill_email(user)
-    @cart = User.cart
+    @cart = user.cart
     @user = user
     @url  = 'https://pastek-media-store.herokuapp.com/' 
     mail(to: @user.email, subject: "Votre commande chez pas'tech team") 
